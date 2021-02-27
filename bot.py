0b9490e75cc7b2bd4f4f47891a60b93e86147c6e
Mihai Pirvulet <mitapirvuet@gmail.com>
2021-02-27 12:32:05 +0200

    add infrastructure for reacting on commands

--- bot.py
+++ bot.py
@@ -1,18 +1,40 @@
 # bot.py
 import os
-
 import discord
+from commands import init_bot
 from dotenv import load_dotenv
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+DEFAULT_CHANNEL = 'general'
 
 intents = discord.Intents.default()
 intents.members = True
+intents.presences = True
 
 client = discord.Client(intents=intents)
 
+commandsFunctions = {
+    'init': init_bot.initBot,
+}
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!mitra '):
+        command = message.content.split(' ')
+
+        if command[1] in commandsFunctions:
+            response = commandsFunctions[command[1]](message)
+
+            if response != None:
+                await message.channel.send(response)
+        else:
+            await message.channel.send(f"!mitra {command[1]} is not a recognized command!")
 @client.event
 async def on_ready():
     for guild in client.guilds:
@@ -24,7 +46,12 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})\n'
     )
 
-    members = '\n - '.join([member.name for member in guild.members])
+    online_users = list(filter(
+        lambda m : m.status != discord.Status.offline,
+        guild.members
+    ))
+
+    members = '\n - '.join([member.name for member in online_users])
     print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
