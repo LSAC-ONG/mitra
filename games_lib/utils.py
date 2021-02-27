7f85f8510f53aa0e672894202090d32c9ceb885a
alexandruanica <anicagabriel1@gmail.com>
2021-02-27 18:48:29 +0200

    create and clear rooms

--- games_lib/utils.py
+++ games_lib/utils.py
@@ -1,5 +1,10 @@
-async def create_and_move(channels, message):
-    for i,channel in enumerate(channels):
-        voice_channel = await message.guild.create_voice_channel(f"Mitra_Room_{i+1}")
+async def create_and_move(bot, channels, message):
+    rooms = bot["rooms"]
+    while len(rooms) < len(channels):
+        voice_channel = await message.guild.create_voice_channel(f"Mitra_Room_{len(rooms) + 1}")
+        bot["rooms"].append(voice_channel)
+
+
+    for i, channel in enumerate(channels):
         for member in channel:
-            await member.move_to(voice_channel)
+            await member.move_to(bot["rooms"][i])
