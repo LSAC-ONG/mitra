194ab26749fdc64d1607f3ec135c53f7385531de
Aleandro2000 <carmicialexandru@yahoo.com>
2021-02-28 15:57:56 +0200

    adaugari, actualizari si emoji la mesaje

--- games_lib/utils.py
+++ games_lib/utils.py
@@ -4,7 +4,6 @@ async def create_and_move(bot, channels, message):
         voice_channel = await message.guild.create_voice_channel(f"Mitra_Room_{len(rooms) + 1}")
         bot["rooms"].append(voice_channel)
 
-
     for i, channel in enumerate(channels):
         for member in channel:
             await member.move_to(bot["rooms"][i])
